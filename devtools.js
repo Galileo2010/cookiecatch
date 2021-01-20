74d7e125c50322c97accaefe4665e37777d74338
Xiaojun Luo <1084305473@qq.com>
2021-01-21 01:47:49 +0800

    [xiaojun] BUG修复

--- devtools.js
+++ devtools.js
@@ -1,9 +1,15 @@
-var domains = localStorage.getItem('domains');
-domains = domains.split(';').map(domain => `(^${domain})`).join('|');
-var eval = `/${domains}/.test(document.domain)`;
-
-chrome.devtools.inspectedWindow.eval(eval, (reuslt, isException) => {
-    if(reuslt && !isException){
-        chrome.devtools.panels.create('CookieCaptor', null, 'panel.html');
+chrome.storage.sync.get('domains', (data) => {
+    var domains = data['domains'];
+    if(!domains){
+        domains = 'stackoverflow.com';
     }
+    domains = domains.split(';').map(domain => `(^${domain})`).join('|');
+    var eval = `/${domains}/.test(document.domain)`;
+
+    chrome.devtools.inspectedWindow.eval(eval, (reuslt, isException) => {
+        if(reuslt && !isException){
+            chrome.devtools.panels.create('CookieCaptor', null, 'panel.html');
+        }
+    });
 });
+
